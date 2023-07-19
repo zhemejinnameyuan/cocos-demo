715791204acc680d59bb05a746e52f1c6eb295b8
zhizhu <zhizhu@gmail.com>
2023-07-19 21:59:12 +0800

    sync

--- game/assets/Script/ButtonController.ts
+++ game/assets/Script/ButtonController.ts
@@ -1,26 +1,22 @@
-import { _decorator, Component, director, Node } from 'cc'; 
+import { _decorator, Component, director, Node } from 'cc';
 const { ccclass, property } = _decorator;
-
 
 @ccclass('ButtonController')
 export class ButtonController extends Component {
     start() {
 
-        
+
     }
 
     update(deltaTime: number) {
 
     }
 
-    click(event, customData :string) { 
+    click(event, customData: string) {
+
+
         const Logger = CLogger.getLogger()
-    
-        Logger.info("this is info")
-        Logger.warn("this is warn")
-        Logger.error("this is error")
- 
-        console.log(customData)
+        Logger.log(customData)
         switch (customData) {
             case 'startUp':
                 director.loadScene('startUp')
@@ -29,7 +25,7 @@ export class ButtonController extends Component {
                 director.loadScene('game')
                 break
         }
- 
+
     }
 }
 
--- game/assets/Script/StartUpController.ts
+++ game/assets/Script/StartUpController.ts
@@ -1,16 +1,53 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, AudioClip, AudioSource, Button, Component, find, Node, resources, SpriteAtlas } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('StartUpController')
 export class StartUpController extends Component {
+
+    public audio = null
+
+    //当前是否播放背景音乐
+    public playBgMusic:Boolean = true
+ 
     start() {
         //初始化,日志开关
         CLogger.init('info')
 
+        this.audio = this.node.getComponent(AudioSource)
+        this.audio.play()
+
+      
+    }
+
+    setBgVolume(res) {
+        this.audio.volume = res._progress
+    }
+
+    //背景音乐关闭/开启
+    bgMusicSwitch(event){
+        let buttonNode = find('Canvas/bg/musicSwitch').getComponent(Button)
+       
+
+        if(!this.playBgMusic){
+            //开启
+            this.playBgMusic = true
+            this.audio.play()
+
+            resources.load('images/login/setting',SpriteAtlas,(error,SpriteAtlas)=>{            
+                buttonNode.normalSprite =SpriteAtlas.spriteFrames.setting6
+            })
+        }else{
+            //关闭
+            this.playBgMusic = false
+            this.audio.stop()
+            resources.load('images/login/setting',SpriteAtlas,(error,SpriteAtlas)=>{            
+                buttonNode.normalSprite =SpriteAtlas.spriteFrames.setting5
+            })
+        }
     }
 
     update(deltaTime: number) {
-        
+
     }
 }
 
